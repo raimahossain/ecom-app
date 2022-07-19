ec48d8fcbe5607f241808e6769bf03a564d9d338
Raima Hossain <raimahossain@Raimas-iMac.home>
2022-07-19 05:11:58 -0700

    first commit

---
+++ pages/success.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import { BsBagCheckFill, BsFillBagCheckFill } from "react-icons/bs";
+
+import { useStateContext } from "../context/StateContext";
+import { schooPride } from "../lib/utils";
+
+const Success = () => {
+  const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
+
+  useEffect(() => {
+    localStorage.clear();
+    setCartItems([]);
+    setTotalPrice(0);
+    setTotalQuantities(0);
+    schooPride();
+  }, []);
+  return (
+    <div className="success-wrapper">
+      <div className="success">
+        <p className="icon">
+          <BsFillBagCheckFill />
+        </p>
+        <h2>Thank You for your order!</h2>
+        <p className="email-msg">Check your email for the reciept.</p>
+        <p className="description">
+          If you have any questions, please email
+          <a className="email" href="mailto:support@trendywatches.com">
+            support@trendywatches.com
+          </a>
+        </p>
+        <Link href="/">
+          <button type="button" width="300px" className="btn">
+            Continue Shopping..
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Success;
---
+++ sanity-ecom-app/schemas/product.js
@@ -0,0 +1,40 @@
+export default {
+  name: "product",
+  title: "Product",
+  type: "document",
+  fields: [
+    {
+      name: "image",
+      title: "Image",
+      type: "array",
+      of: [{ type: "image" }],
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "slug", //unique string/url
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "name", //unique slug generated based on name property
+        maxLength: 90,
+      },
+    },
+    {
+      name: "price",
+      title: "Price",
+      type: "number",
+    },
+    {
+      name: "details",
+      title: "Details",
+      type: "string",
+    },
+  ],
+};
